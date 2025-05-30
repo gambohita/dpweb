99efc1f8d5a13affc67db41c338b39e33dd71f07
gambohita <gamboabautistae@gmail.com>
2025-05-30 12:19:10 -0500

    puebra 2

---
+++ config/config example.php
@@ -0,0 +1,14 @@
+<?php
+const BD_HOST = 'localhost';
+const BD_NAME = 'inventario'; 
+const BD_USER = 'root';
+const BD_PASSWORD = 'root';
+const BD_CHARSET = 'utf8';
+
+//ingresar link de proyecto
+
+const BASE_URL ='';
+
+
+
+?>
