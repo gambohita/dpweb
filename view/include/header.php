1d30f47ff3b06c857bbcd0445788ad467a2b4a77
gambohita <gamboabautistae@gmail.com>
2025-07-08 11:41:06 -0500

    76

--- view/include/header.php
+++ view/include/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GAMBOA</title>
+    <title>Sis Venta </title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
     <script>
         const base_url = '<?php echo BASE_URL; ?>';
@@ -63,44 +63,3 @@
             </div>
         </div>
     </nav>
-   <!--inicio de acuerdo de pagina-->
-    <div class="container" style="margin-top: 100px;">
-        <div class="card">
-            <div class="card-header" style="text-align:center;">
-                Registrar Categoria
-            </div>
-            <form id="frm_categoria" action="" method="">
-                <div class="card-body">
-
-                    <div class="mb-3 row">
-                        <label for="nombre" class="col-sm-2 col-form-label">Nombre</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="nombre" name="nombre" required>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="detalle" class="col-sm-2 col-form-label">Detalle</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="detalle" name="detalle" required>
-                        </div>
-                    </div>
-
-                    <div style="display: flex; justify-content:center; gap:20px">
-                        <button type="submit" class="btn btn-primary">Registrar</button>
-                        <button type="reset" class="btn btn-info">Limpiar</button>
-                        <button type="button" class="btn btn-danger">Cancelar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-     <!--fin de acuerdo de pagina-->
-    
-
-</body>
-<script src="<?php echo BASE_URL; ?>view/function/categoria.js"></script>
-<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-
-</html>
--- view/new-user.php
+++ view/new-user.php
@@ -1,71 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GAMBOA</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
-    <script>
-        const base_url = '<?php echo BASE_URL; ?>';
-    </script>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">users</a>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">products</a>
-                    </li>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">categories</a>
-                    </li>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">clients</a>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">shops</a>
-                    </li>
-                    <a class="nav-link " aria-current="page" href="#">sales</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true"></a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <!-- inicio de cuerpo de pagina -->
     <div class="container-fluid">
         <div class="card">
-            <h5 class="card-header">TITULO</h5>
+            <h5 class="card-header">Registro de Usuario</h5>
             <form id="frm_user" action="" method="">
                 <div class="card-body">
                     <div class="mb-3 row">
@@ -154,13 +91,5 @@
             </form>
         </div>
     </div>
-
-    
-
-</body>
-<script src="<?php echo BASE_URL; ?>view/function/user.js"></script>
-<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-
-</html>
+    <!-- fin de cuerpo de pagina -->
+     <script src="<?php echo BASE_URL; ?>view/function/user.js"></script>
