f61130d78b3510a151160c02114d1ed34d8fa42e
gambohita <gamboabautistae@gmail.com>
2025-11-05 12:50:56 -0500

    ...

--- model/views_model.php
+++ model/views_model.php
@@ -2,7 +2,7 @@
 class viewModel{
     protected static function get_view($view)
     {
-         $white_list = ["home", "products", "users", "new-user", "categories", "update", "edit-producto", "edit-categoria", "categoria", "produc", "new-cliente", "new-proveedor", "proveedor", "cliente", "edit-client", "edit-proveedor"];
+         $white_list = ["home", "products", "users", "new-user", "categories", "update", "vista-cliente", "edit-producto", "edit-categoria", "categoria", "produc", "new-cliente", "new-proveedor", "proveedor", "cliente", "edit-client", "edit-proveedor"];
         if (in_array($view, $white_list)){
             if(is_file("./view/".$view.".php")){
                 $content ="./view/".$view.".php";
