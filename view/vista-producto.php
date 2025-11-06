5c60e3a95909a6fa25c46371547aa838fbdb7d17
gambohita <gamboabautistae@gmail.com>
2025-11-06 01:22:53 -0500

    actualización

--- view/vista-producto.php
+++ view/vista-producto.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Vista Productos</title>
-  <link rel="stylesheet" href="http://localhost/dpweb/view/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="view/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -11,14 +11,13 @@
       PRODUCTOS EN LÍNEA
   </h4>
 
-  <!-- Contenedor de productos -->
   <div class="container mt-5">
       <div id="contenedor_productos" class="row"></div>
   </div>
 
-  <!-- Scripts al final -->
-  <script src="http://localhost/dpweb/view/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="http://localhost/dpweb/view/function/products-vista.js"></script>
+  <!-- Scripts -->
+  <script src="view/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="view/function/products-vista.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </body>
